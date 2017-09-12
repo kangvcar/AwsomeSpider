15d5dbd692b0f7ba264df775a9f34728a106931a
kangvcar <kangvcar@126.com>
2017-09-12 19:54:04 +0800

    1.增加了一些游戏提示

--- NumGame5.py
+++ NumGame5.py
@@ -11,24 +11,29 @@ namedict = {}
 names = []
 a = 0
 kcNum = 1
+lun = 1
 
 def inputName(namestr):
     for i in namestr.split(','):
         namedict[i] = 0
 
+
+
 pkNum = int(raw_input('请主持人确定PK次数:'))
-print '请主持人确定猜数字的范围'
 sNum = int(raw_input('请主持人确定猜数字的范围最小值:'))
 bNum = int(raw_input('请主持人确定猜数字的范围最大值:'))
+kcNum = int(raw_input('请主持人确定每次可以猜的次数(默认4次):'))
 namestring = raw_input('请每位参赛者按顺序输入自己的名字(用逗号,分割):')
 inputName(namestring)
-kcNum = int(raw_input('请主持人确定每次可以猜的次数(默认4次):'))
 
 while pkNum > 0:
+
     suiji = random.randint(sNum, bNum)
+    print '===第', lun, '轮==='
     for x in range(kcNum):
+        print '===第', lun, '轮==>第', x + 1, '次'
         for j in namedict:
-            print j
+            print j, '玩家:'
             num = int(raw_input('your number is:'))
             if num == suiji:
                 print 'you win!'
@@ -40,10 +45,13 @@ while pkNum > 0:
                 print 'too big'
             else:
                 print 'too small'
-    if a == 0:
-        print 'game over'
-        print 'result number is:', suiji
+        if a == 0:
+            print 'game over'
+            print 'result number is:', suiji
+            suiji = random.randint(sNum, bNum)
     pkNum = pkNum - 1
+    if pkNum > 0:
+        lun = lun + 1
 
 print '-----------'
 print '---score---'
