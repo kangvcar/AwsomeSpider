6745240806594133081710d5e89c460561ddac4f
kangvcar <kangvcar@126.com>
2017-09-19 10:13:57 +0800

    add common

--- Youdao-reptile.py
+++ Youdao-reptile.py
@@ -2,9 +2,10 @@
 
 #import urllib
 import urllib2
-
+#爬取网页
 response = urllib2.urlopen('http://fanyi.youdao.com')
 html = response.read()
+#写入文件
 f = open('Youdao.txt', 'w')
 f.write(html)
 f.close()
