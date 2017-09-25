5c0919ecdd153b55340df9971e860b439e307004
kangvcar <kangvcar@126.com>
2017-09-25 16:15:15 +0800

    上一个版本失败，应该是headers验证的问题，我们加上一个headers验证，成功返回了第一页的HTML代码

--- Spiders/Spider_qiushibaike.py
+++ Spiders/Spider_qiushibaike.py
@@ -10,8 +10,10 @@ import urllib2
 
 page = 1
 url = 'https://www.qiushibaike.com/hot/page/' + str(page)
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = {'User-Agent': user_agent}
 try:
-	request = urllib2.Request(url)
+	request = urllib2.Request(url, headers = headers)
 	response = urllib2.urlopen(request)
 	print response.read()
 except urllib2.URLError, e:
