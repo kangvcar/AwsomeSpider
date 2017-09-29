c9986a4efefc7e6974f4b8407c8d0b3b3acae32f
kangvcar <kangvcar@126.com>
2017-09-29 17:33:31 +0800

    发送纯文本邮件，包含主题，发件人，收件人等信息显示

--- Other/Send_email.py
+++ Other/Send_email.py
@@ -5,10 +5,19 @@
 # @Link    : http://www.github.com/kangvcar/
 # @Version : $Id$
  
-import smtplib
+from email import encoders
 from email.mime.text import MIMEText
-# 构造邮件
-msg = MIMEText('hello, send by Python...123', 'plain', 'utf-8')
+from email.header import Header
+from email.utils import parseaddr, formataddr
+import smtplib
+
+#函数_format_addr()用来格式化一个邮件地址
+def _format_addr(s):
+	name, addr = parseaddr(s)
+	return formataddr((\
+		Header(name, 'utf-8').encode(),\
+		addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
 # 输入Email地址和口令:
 from_addr = raw_input('From:')	#kangvcar123@163.com
 password = raw_input('Password:')	#xxxxxxxx
@@ -16,6 +25,12 @@ password = raw_input('Password:')	#xxxxxxxx
 smtp_server = raw_input('SMTP server:')	#smtp.163.com
 # 输入收件人地址:
 to_addr = raw_input('To:') 	##kangvcar123@163.com
+# 构造邮件
+msg = MIMEText('hello, send by Python...123', 'plain', 'utf-8')
+
+msg['From'] = _format_addr(u'Python爱好者<%s>' % from_addr)
+msg['To'] = _format_addr(u'管理员<%s>' % to_addr)
+msg['Subject'] = Header(u'来自SMTP的问候······', 'utf-8').encode()
 
 server = smtplib.SMTP(smtp_server, 25) 	# SMTP协议默认端口是25
 server.set_debuglevel(1)		#可以打印出和SMTP服务器交互的所有信息
