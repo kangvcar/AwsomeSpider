b48b4f3864fde0dd866e28f4ec0f50d63618c7fd
kangvcar <kangvcar@126.com>
2017-10-17 13:53:13 +0800

    爬取直播吧最新NBA新闻

---
+++ Spiders/zhibo8/Spider_zhibo8_news.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-10-17 09:45:36
+# @Author  : kangvcar (kangvcar@126.com)
+# @Link    : http://www.github.com/kangvcar/
+# @Version : $Id$
+
+import urllib2
+from lxml import html
+
+class newsItem(object):
+	news_title = None
+	news_link = None
+	news_source = None
+	news_time = None
+	news_info = None
+	
+
+class get_news_zhibo8_info(object):
+	def __init__(self, url):
+		self.url = 'https://news.zhibo8.cc/nba/more.htm'
+		self.page_title = self.get_page_title(self.url)
+		self.news_title_lists = self.get_news_title_list(self.url)
+		# self.news_info_lists = self.get_news_info_list(self.news_title_lists)
+		self.print_news_title(self.news_title_lists)
+		# self.print_news_info(self.news_info_lists)
+		
+	
+
+	def get_page_title(self, url):
+		'''获取页面的大标题'''
+		selector = self.get_source_page(url)
+		page_title = selector.xpath('//div[@id="boxlist"]/div[@class="title"]/span/text()')[0]
+		return page_title
+		
+	def get_news_title_list(self, url):
+		'''获取页面的所有新闻标题'''
+		selector = self.get_source_page(url)
+		news_lists = selector.xpath('//div[@id="boxlist"]//div[@class="dataList"]//ul[@class="articleList"]//li')
+		news_items = []
+		for news_list in news_lists:
+			item = newsItem()
+			try:
+				item.news_title = news_list.xpath('./span[@class="articleTitle"]/a/text()')[0]
+				item.news_link = 'http:' + news_list.xpath('./span[@class="articleTitle"]/a/@href')[0]
+				item.news_source = news_list.xpath('./span[@class="source"]/text()')[0]
+				item.news_time = news_list.xpath('./span[@class="postTime"]/text()')[0]
+			except:
+				continue
+			else:
+				news_items.append(item)
+		return news_items
+		
+	def get_news_info_list(self, news_title_lists):
+		news_info_lists = []
+		for news in news_title_lists:
+			news_info_items = newsItem()
+			selector = self.get_source_page(news.news_link)
+			news_info_items.news_info = selector.xpath('.//div[@id="signals"]//p/text()')		#list
+			news_info_items.news_title = news.news_title
+			news_info_items.news_link = news.news_link
+			news_info_items.news_source = news.news_source
+			news_info_items.news_time = news.news_time
+			news_info_lists.append(news_info_items)
+		return news_info_lists
+			
+	def print_news_title(self, news_title_lists):
+		'''打印所有新闻标题'''
+		for new_title_list in news_title_lists:
+			print u'新闻标题 >>> ' + new_title_list.news_title
+			print u'新闻链接 >>> ' + new_title_list.news_link
+			print u'新闻来源 >>> ' + new_title_list.news_source
+			print u'发布时间 >>> ' + new_title_list.news_time
+			print '>>>>>>>>>>>>>>>>' * 5
+			
+	def print_news_info(self, news_info_lists):
+		'''打印所有新闻的详细内容'''
+		for news_info_list in news_info_lists:
+			print u'新闻标题 >>> ' + news_info_list.news_title
+			print u'新闻链接 >>> ' + news_info_list.news_link
+			print u'新闻内容 >>> ' + '\n'.join(news_info_list.news_info)
+			print u'新闻来源 >>> ' + news_info_list.news_source
+			print u'发布时间 >>> ' + news_info_list.news_time
+			print '>>>>>>>>>>>>>>>>' * 5
+		
+	def get_source_page(self, url):
+		'''获取网页源码，并使用lxml格式化源码'''
+		try:
+			request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+			request = urllib2.Request(url, None, request_headers)
+			response = urllib2.urlopen(request).read()
+			selector = html.fromstring(response)
+		except:
+			selector = None
+		finally:
+			return selector
+
+zhibo8 = get_news_zhibo8_info('https://news.zhibo8.cc/nba/more.htm')
+#https://news.zhibo8.cc/nba/more.htm
