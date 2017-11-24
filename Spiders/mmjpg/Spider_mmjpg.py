0db5ba99fb17858ae8e34a6579401c5599e35207
kangvcar <kangvcar@126.com>
2017-11-24 12:51:47 +0800

    多进程，有BUG

--- Spiders/mmjpg/Spider_mmjpg.py
+++ Spiders/mmjpg/Spider_mmjpg.py
@@ -8,18 +8,25 @@
 import urllib2
 from lxml import html
 import os
+from multiprocessing import Process
 
 class get_mm_pic(object):
 	def __init__(self, url):
-		self.url = url
-		self.sumpage = self.get_sum_page(self.url)
-		print u'总共检测到有' + self.sumpage + u'页'
-		self.urls = self.get_all_page_urls(self.sumpage)
-		print u'已获取所有页面的url'
-		self.page_links = self.get_page_links(self.urls)
-		print u'已获取所有图集的link'
-		self.download_all_pic(self.page_links)
-		print u'已下载所有图片'
+		if __name__ == '__main__':
+			self.url = url
+			self.sumpage = self.get_sum_page(self.url)
+			print u'总共检测到有' + self.sumpage + u'页'
+			self.urls = self.get_all_page_urls(self.sumpage)
+			print u'已获取所有页面的url'
+			self.page_links = self.get_page_links(self.urls)
+			print u'已获取所有图集的link'
+			# self.download_all_pic(self.page_links)
+			for each_10_list in [self.page_links[i:i+5] for i in range(0, len(self.page_links), 5)]:
+					for album_page in each_10_list:
+						p = Process(target=self.download_all_pic, args=(album_page,))
+						p.start()
+						# p.join()
+			print u'已下载所有图片'
 		
 	def get_sum_page(self, url):
 		''' 获取总页数'''
@@ -51,36 +58,37 @@ class get_mm_pic(object):
 				page_links.append(li)
 		return page_links
 	
-	def download_all_pic(self, page_links):
+	# def download_all_pic(self, page_links):
+	def download_all_pic(self, page_link):
 		''' 进入所有图集，并下载所有图片 '''
-		for page_link in page_links:
-			''' 进入单个图集'''
+		# for page_link in page_links:
+			# ''' 进入单个图集'''
+		try:
+			selector = self.get_source_page(page_link)
+			album_title = selector.xpath('//div[@class="article"]/h2/text()')[0]
+			# album_title 为图集的标题，用于命名文件夹
+			sum_pic = selector.xpath('//div[@id="page"]/a[last()-1]/text()')[0]
+			# sum_pic 为图集的总图片数量
+			path = self.mk_pic_dir(album_title)
+			# 以图集的标题为文件夹名 创建 文件夹
+		except:
+			pass
+		for pic in range(1, int(sum_pic)+1):
 			try:
-				selector = self.get_source_page(page_link)
-				album_title = selector.xpath('//div[@class="article"]/h2/text()')[0]
-				# album_title 为图集的标题，用于命名文件夹
-				sum_pic = selector.xpath('//div[@id="page"]/a[last()-1]/text()')[0]
-				# sum_pic 为图集的总图片数量
-				path = self.mk_pic_dir(album_title)
-				# 以图集的标题为文件夹名 创建 文件夹
+				print u'正在下载-->' + album_title + u'-->第' + str(pic) + u'张美图...'
+				pic_link = page_link + '/' + str(pic)
+				src = self.get_pic_link(pic_link)
+				filename = '%s.jpg' % (pic)
 			except:
 				continue
-			for pic in range(1, int(sum_pic)+1):
+			else:
 				try:
-					print u'正在下载-->' + album_title + u'-->第' + str(pic) + u'张美图...'
-					pic_link = page_link + '/' + str(pic)
-					src = self.get_pic_link(pic_link)
-					filename = '%s.jpg' % (pic)
+					req = urllib2.Request(src)
+					req.add_header('Referer','http://img.mmjpg.com/')
+					with open(path + '/' + filename, 'wb') as fp:
+						fp.write(urllib2.urlopen(req, timeout=3).read())
 				except:
 					continue
-				else:
-					try:
-						req = urllib2.Request(src)
-						req.add_header('Referer','http://img.mmjpg.com/')
-						with open(path + '/' + filename, 'wb') as fp:
-							fp.write(urllib2.urlopen(req, timeout=3).read())
-					except:
-						continue
 	
 	def mk_pic_dir(self, dirname):
 		''' 用图集名创建文件夹 '''
