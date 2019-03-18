b282a2325f145c2f95a12007855d783c141cc3b8
Kangvcar <kangvcar@126.com>
2019-03-18 22:15:09 +0800

    Update Spider_135zy.py

--- Spiders/Spider_135zy.py
+++ Spiders/Spider_135zy.py
@@ -7,13 +7,12 @@
 from lxml import etree
 import requests
 import pymysql.cursors
+import threading
 
 class movieInfo(object):
     '''define movieInfo Class to save movie info'''
-    Id = None   # INCREMENT
     mId = None  # 影片ID
     mName = None    # 影片名
-    mAlias = None   # 影片别名
     mCover = None  # 影片封面
     mScore = None   # 影片评分
     mDirector = None    # 影片导演
@@ -21,7 +20,6 @@ class movieInfo(object):
     mType = None    # 影片类型
     mArea = None    # 影片地区
     mYear = None    # 影片上映年份
-    mUpdate = None  # 影片更新时间
     mSumary = None  # 影片介绍
     mPlayUrl_m3u8 = None    # 影片播放链接，m3u8格式
     mPlayUrl_mp4 = None     # 影片播放链接，mp4格式
@@ -43,7 +41,7 @@ class zySpider(object):
     def getpageUrls(self, startUrl, sumPage):
         '''生成所有影片列表页的Url'''
         pageUrlsList = []
-        for i in range(3, sumPage+1):
+        for i in range(1, sumPage+1):
             pageUrl = startUrl + '-pg-' + str(i) + '.html'
             # print(pageUrl)
             pageUrlsList.append(pageUrl)
@@ -63,33 +61,43 @@ class zySpider(object):
                 MovieUrlsList.append(url)
         return MovieUrlsList
 
+    def getMovieDetail(self, detailUrl):
+        '''获取一部影片的详情信息'''
+        item = movieInfo()
+        html = self.getSourceCode(detailUrl)
+        item.mId = detailUrl.split('-')[-1].split('.')[0]
+        item.mName = html.xpath('//div[@class="vodInfo"]//h2')[0].text
+        item.mScore = html.xpath('//div[@class="vodInfo"]//label')[0].text
+        # item.mScore = ' '
+        item.mDirector = html.xpath('//div[@class="vodinfobox"]/ul//li[2]//span')[0].text + ' '
+        # item.mDirector = ' '
+        item.mStar = html.xpath('//div[@class="vodinfobox"]/ul//li[3]//span')[0].text + ' '
+        # item.mStar = ' '
+        item.mType = html.xpath('//div[@class="vodinfobox"]/ul//li[4]//span')[0].text
+        item.mArea = html.xpath('//div[@class="vodinfobox"]/ul//li[5]//span')[0].text + ' '
+        item.mYear = html.xpath('//div[@class="vodinfobox"]/ul//li[7]//span')[0].text
+        item.mCover = html.xpath('//img[@class="lazy"]/@src')[0]
+        item.mSumary = html.xpath('//*[@class="vodplayinfo"][1]//text()')[0][:200] + '...'
+        # item.mSumary = ' '
+        item.mPlayUrl_mp4 = html.xpath('//*[@class="vodplayinfo"][last()]//ul[1]/li/input/@value')[0]
+        item.mPlayUrl_m3u8 = html.xpath('//*[@class="vodplayinfo"][last()]//ul[2]/li/input/@value')[0]
+        # print(">>> 已成功爬取电影：" + item.mName + "(" + item.mId + ")")
+        return item
+
     def getMovieInfoItems(self, movieUrls):
         '''获取所有影片详情信息'''
         items = []
         for i, infourl in enumerate(movieUrls):
             try:
-                item = movieInfo()
-                html = self.getSourceCode(infourl)
-                item.mId = infourl.split('-')[-1].split('.')[0]
-                item.mName = html.xpath('//div[@class="vodInfo"]//h2')[0].text
-                item.mScore = html.xpath('//div[@class="vodInfo"]//label')[0].text
-                item.mAlias = html.xpath('//div[@class="vodinfobox"]/ul//li[2]//span')[0].text + ' '
-                item.mDirector = html.xpath('//div[@class="vodinfobox"]/ul//li[2]//span')[0].text + ' '
-                item.mStar = html.xpath('//div[@class="vodinfobox"]/ul//li[3]//span')[0].text + ' '
-                item.mType = html.xpath('//div[@class="vodinfobox"]/ul//li[4]//span')[0].text + ' '
-                item.mArea = html.xpath('//div[@class="vodinfobox"]/ul//li[5]//span')[0].text + ' '
-                item.mYear = html.xpath('//div[@class="vodinfobox"]/ul//li[7]//span')[0].text + ' '
-                item.mUpdate = html.xpath('//div[@class="vodinfobox"]/ul//li[8]//span')[0].text + ' '
-                item.mCover = html.xpath('//img[@class="lazy"]/@src')[0]
-                item.mSumary = html.xpath('//*[@class="vodplayinfo"][1]//text()')[0] + ' '
-                # print(item.mSumary)
-                item.mPlayUrl_mp4 = html.xpath('//*[@class="vodplayinfo"][last()]//ul[1]/li/input/@value')[0] + ' '
-                item.mPlayUrl_m3u8 = html.xpath('//*[@class="vodplayinfo"][last()]//ul[2]/li/input/@value')[0] + ' '
+                item = self.getMovieDetail(infourl)
+                # t = threading.Thread(target=self.getMovieDetail, args=(infourl,))
+                # t.start()
+                # t.join()
                 items.append(item)
                 print(">>> 已成功爬取电影：" + str(i+1) + '.' + item.mName + "(" + item.mId + ")")
             except Exception as e:
                 continue
-            # if i==5:
+            # if i>5:
             #     break
         return items
 
@@ -99,17 +107,16 @@ class zySpider(object):
         connection = pymysql.connect(host='localhost',
                                      user='root',
                                      password='root',
-                                     db='zy135movie',
+                                     db='okmovie3',
                                      cursorclass=pymysql.cursors.DictCursor)
         try:
             with connection.cursor() as cursor:
                 # Create a new record
-                sql = "INSERT INTO `movie` (`mId`, `mName`, `mAlias`, `mCover`, `mScore`,`mDirector`,`mStar`,`mType`,`mArea`,`mYear`,`mUpdate`,`mSumary`,`mPlayUrl_m3u8`,`mPlayUrl_mp4`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                sql = "INSERT INTO `movie` (`mId`, `mName`, `mCover`, `mScore`,`mDirector`,`mStar`,`mType`,`mArea`,`mYear`,`mSumary`,`mPlayUrl_m3u8`,`mPlayUrl_mp4`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
                 for item in items:
                     try:
                         mId = item.mId
                         mName = item.mName.encode('utf-8')
-                        mAlias = item.mAlias.encode('utf-8')
                         mCover = item.mCover.encode('utf-8')
                         mScore = item.mScore
                         mDirector = item.mDirector.encode('utf-8')
@@ -117,11 +124,10 @@ class zySpider(object):
                         mType = item.mType.encode('utf-8')
                         mArea = item.mArea.encode('utf-8')
                         mYear = item.mYear.encode('utf-8')
-                        mUpdate = item.mUpdate.encode('utf-8')
                         mSumary = item.mSumary.encode('utf-8')
                         mPlayUrl_mp4 = item.mPlayUrl_mp4.encode('utf-8')
                         mPlayUrl_m3u8 = item.mPlayUrl_m3u8.encode('utf-8')
-                        cursor.execute(sql, (mId, mName,mAlias,mCover,mScore,mDirector,mStar,mType,mArea,mYear,mUpdate,mSumary,mPlayUrl_mp4,mPlayUrl_m3u8))
+                        cursor.execute(sql, (mId, mName,mCover,mScore,mDirector,mStar,mType,mArea,mYear,mSumary,mPlayUrl_mp4,mPlayUrl_m3u8))
                     except Exception as e:
                         print('写入失败：'+mName)
                         continue
@@ -138,7 +144,7 @@ class zySpider(object):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
         try:
-            html = etree.HTML(requests.get(url, headers=headers).text.encode(
+            html = etree.HTML(requests.get(url, headers=headers, timeout=2).text.encode(
                 'ISO-8859-1', "ignore").decode('utf-8', "ignore"))
         except Exception as e:
             return None
@@ -147,5 +153,5 @@ class zySpider(object):
 
 # Arg1: 指定爬取的页数
 # Arg2: 指定爬取影片类型
-# [电影，连续剧，综艺，动漫，伦理片，美女热舞，VIP视频秀，纪录片，街拍美女]
-Spider = zySpider(3, '电影')
+# [电影，连续剧，综艺，动漫，伦理片，美女热舞写真，VIP视频秀，纪录片，街拍美女]
+Spider = zySpider(2, '电影')
